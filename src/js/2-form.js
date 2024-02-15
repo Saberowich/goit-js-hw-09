78abae988a1b86fffd99abf47a71688336c915bd
Bohdan Babych <babychbohdan0@gmail.com>
2024-02-15 14:18:22 +0100

    done hw09

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -4,12 +4,17 @@ const STORAGE_KEY = 'feedback-form-state';
   
 const storageForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
-populateText();
+textOnRefresh();
 
 
 form.addEventListener("input", checkUserMessage); 
 
 function checkUserMessage() {
+const formData = {
+      email: form.elements.email.value.trim(),
+      message: form.elements.message.value.trim(),
+    };
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
   form.addEventListener("submit", alertMessageForUser);
@@ -18,15 +23,15 @@ function alertMessageForUser(e) {
   e.preventDefault();
 
   if (form.elements.email.value.trim() === '' || form.elements.message.value.trim() === '') {
-    alert('Please enter your email and message');
+    alert('Please enter both your email and message');
     return;
   }
-  console.log("Data submit:", JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  console.log("Submitted data:", JSON.parse(localStorage.getItem(STORAGE_KEY)));
     e.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
 }
 
-function populateText() {
+function textOnRefresh() {
    const savedMessage = localStorage.getItem(STORAGE_KEY);
 
   if (savedMessage) {
